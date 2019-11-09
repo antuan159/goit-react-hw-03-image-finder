fed3313504c6e643fde95fac4560f8c78fc05103
antuan159 <lanos159@ukr.net>
2019-11-09 23:58:19 +0200

    Updates

---
+++ precache-manifest.99032adf5cc6c8ffe71de5fe68b37ee9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18e59de1fec4f2ff0a0acf7e14934928",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "d0afef681baad6dd001f",
+    "url": "/goit-react-hw-03-image-finder/static/css/2.04baf729.chunk.css"
+  },
+  {
+    "revision": "8c9b619425ee21c621a6",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.34bc2dc3.chunk.css"
+  },
+  {
+    "revision": "d0afef681baad6dd001f",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.b651ab2d.chunk.js"
+  },
+  {
+    "revision": "8c9b619425ee21c621a6",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.12ebddc1.chunk.js"
+  },
+  {
+    "revision": "85f17568e4db941861cc",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.68d0c4c8.js"
+  }
+]);
